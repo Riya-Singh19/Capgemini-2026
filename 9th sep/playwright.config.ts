de7e702cc40c31d34e1ee8570c0f1f1517657a34
Riya Singh <riya6.singh12@gmail.com>
2026-09-09 12:58:24 +0530

    Change test directory to '9th sep'

--- 9th sep/playwright.config.ts
+++ 9th sep/playwright.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig, devices } from '@playwright/test';
 
 export default defineConfig({
-  testDir: './tests',
+  testDir: './tests/9th sep',
   use: {
     baseURL: 'https://phptravels.net/',
     headless: true,
