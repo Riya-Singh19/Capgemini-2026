9482fdc46b1443e44b4d02dc721e3cadf20e9024
Riya Singh <riya6.singh12@gmail.com>
2026-09-09 14:13:28 +0530

    Update playwright.config.ts

--- 9th sep/playwright.config.ts
+++ 9th sep/playwright.config.ts
@@ -1,14 +1,19 @@
 import { defineConfig, devices } from '@playwright/test';
 
+// export default defineConfig({
+//   testDir: './tests/9th sep',
+//   use: {
+//     baseURL: 'https://phptravels.net/',
+//     headless: true,
+//     screenshot: 'only-on-failure',
+//     trace: 'retain-on-failure',
+//     video: 'retain-on-failure'
+//   },
 export default defineConfig({
-  testDir: './tests/9th sep',
-  use: {
-    baseURL: 'https://phptravels.net/',
-    headless: true,
-    screenshot: 'only-on-failure',
-    trace: 'retain-on-failure',
-    video: 'retain-on-failure'
-  },
+  testDir: '.',
+});
+
+  
   projects: [
     {
       name: 'chromium',
