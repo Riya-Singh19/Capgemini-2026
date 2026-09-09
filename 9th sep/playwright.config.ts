f317eecba1c977df850b3c46c301db8f31d0b261
Riya Singh <riya6.singh12@gmail.com>
2026-09-09 14:47:13 +0530

    Update Playwright config for test directory and options

--- 9th sep/playwright.config.ts
+++ 9th sep/playwright.config.ts
@@ -1,19 +1,15 @@
 import { defineConfig, devices } from '@playwright/test';
 
-// export default defineConfig({
-//   testDir: './tests/9th sep',
-//   use: {
-//     baseURL: 'https://phptravels.net/',
-//     headless: true,
-//     screenshot: 'only-on-failure',
-//     trace: 'retain-on-failure',
-//     video: 'retain-on-failure'
-//   },
 export default defineConfig({
   testDir: '.',
-});
 
-  
+  use: {
+    headless: true,
+    screenshot: 'only-on-failure',
+    trace: 'retain-on-failure',
+    video: 'retain-on-failure'
+  },
+
   projects: [
     {
       name: 'chromium',
